20cc144673b3ff9f672eed3410ab19a349745fdb
Lance Stout <lancestout@gmail.com>
2014-04-09 23:48:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# otalk-chat-view
+
+A companion module to `otalk-client`, this module provides all of the logic
+necessary for implementing a chat view. You only need to provide the templates.
+
+## Installing
+
+```sh
+$ npm install otalk-chat-view
+```
+
+## Using
+
+```javascript
+var templates = require('./templates');
+var BasePage = require('./base');
+
+var createChatView = require('otalk-chat-view');
+var ChatPage = createChatView(BasePage, {
+    templates: {
+        base: templates.pages.chat,
+        chat: templates.includes.chat,
+        chatGroup = tempates.includes.chatWrapper
+    }
+});
+```
+
+## View Role Assumptions
+
+- `chat-input`
+
+    A `<div/>` that also contains a `<textarea />` element inside it.
+
+- `message-list`
+
+    A `<ul/>` where all chats will be inserted (where each `<li/>` is for a group
+    of chats all from the same participant).
+
+## Model Assumptions
+
+A `Contact` model of some sort is assumed, which provides:
+
+- `.messages`
+
+    A collection of chat message models.
+
+- `.lastSentMessageID`
+
+    The ID of the last sent message, which is needed to do message correction.
+
+- `.sendChatState(state)`
+
+    Sends a typing/paused notification to the contact.
+
+- `.sendChat(body, replaceID)`
+
+    Creates and sends a new message, optionally marked as a correction edit for
+    the last sent message.
+
+
+A `Message` model is assumed to provide:
+
+- `.type`
+- `.fromFullJID`
+- `.fromBareJID`
+
+In addition, these fields will enable more advanced display options if supported
+by the `Message` model:
+
+- `.mine`
+- `.pendingAck`
+- `.delayed`
+- `.edited`
+- `.pendingReceipt`
+- `.receiptReceived`
+- `.meAction`
+
+## License
+
+MIT
+
+## Created By
+
+If you like this, follow [@lancestout](http://twitter.com/lancestout) on twitter.
---
+++ index.js
@@ -0,0 +1,244 @@
+var _ = require('underscore');
+var domify = require('domify');
+var StayDown = require('staydown');
+var View = require('ampersand-view');
+var State = require('ampersand-state');
+
+
+var ViewState = State.extend({
+    type: 'chatView',
+    session: {
+        editing: 'boolean',
+        typing: 'boolean',
+        paused: 'boolean',
+        active: 'boolean'
+    }
+});
+
+
+module.exports = function (BaseView, options) {
+    var templates = options.templates;
+
+    return BaseView.extend({
+        template: templates.base,
+
+        initialize: function () {
+            this.state = new ViewState();
+
+            this.listenTo(this.state, 'change:active', this.handleActive);
+
+            this.listenTo(this.model.messages, 'change', this.refreshModel);
+            this.listenTo(this.model.messages, 'sort', this.renderCollection);
+
+            this.render();
+        },
+
+        events: {
+            'keydown [role="chat-input"]': 'handleKeyDown',
+            'keyup [role="chat-input"]': 'handleKeyUp'
+        },
+
+        handleActive: function () {
+            if (this.state.active) {
+                this.sendChatState('active');
+            } else {
+                this.sendChatState('gone');
+            }
+        },
+
+        handleKeyDown: function (e) {
+            if (e.which === 13 && !e.shiftKey) {
+                this.sendChat();
+                e.preventDefault();
+            } else if (e.which === 38 && this.$chatInput.value === '' && this.model.lastSentMessageID) {
+                this.state.editing = true;
+                var prev = this.model.messages.find(this.model.lastSentMessageID);
+                if (prev) {
+                    this.$chatInput.value = prev.body;
+                } else {
+                    this.state.editing = false;
+                }
+                e.preventDefault();
+            } else if (e.which === 40 && this.state.editing) {
+                this.state.editing = false;
+                e.preventDefault();
+            } else if (!e.ctrlKey && !e.metaKey) {
+                if (!this.state.typing || this.state.paused) {
+                    this.state.typing = true;
+                    this.state.paused = false;
+                    this.model.sendChatState('composing');
+                }
+            }
+        },
+
+        handleKeyUp: function (e) {
+            this.resizeInput();
+            if (this.state.typing && this.$chatInput.value.length === 0) {
+                this.state.typing = false;
+                this.model.sendChatState('active');
+            } else if (this.state.typing) {
+                this.pausedTyping();
+            }
+        },
+
+        pausedTyping: _.debounce(function () {
+            if (this.state.typing && !this.state.paused) {
+                this.state.paused = true;
+                this.model.sendChatState('paused');
+            }
+        }, 3000),
+
+        render: function () {
+            if (this.rendered) {
+                this.staydown.checkdown();
+                this.resizeInput();
+                return this;
+            }
+
+            var self = this;
+
+            this.rendered = true;
+
+            this.renderAndBind();
+
+            this.registerBindings(this.state, {
+                editing: ['[role="chat-input"]', 'class'],
+                typing: ['[role="chat-input"]', 'class'],
+                paused: ['[role="chat-input"]', 'class']
+            });
+
+            this.$chatInput = this.get('[role="chat-input"] textarea');
+            this.$chatBox = this.get('[role="chat-input"]');
+            this.$messageList = this.get('[role="message-list"]');
+
+            this.staydown = new StayDown({
+                target: this.$messageList,
+                interval: 500
+            });
+
+            this.renderCollection();
+
+            this.listenTo(this.model.messages, 'add', this.handleChatAdded);
+
+            window.addEventListener('resize', _.bind(this.resizeInput, this));
+
+            this.staydown.checkdown();
+            this.resizeInput();
+
+            return this;
+        },
+
+        renderCollection: function () {
+            var self = this;
+
+            // Empty out the messages container
+            while (this.$messageList.firstChild) {
+                this.$messageList.removeChild(this.$messageList.firstChild);
+            }
+
+            this.model.messages.each(function (model, i) {
+                self.appendModel(model);
+            });
+            this.staydown.checkdown();
+        },
+
+        handleChatAdded: function (model) {
+            this.appendModel(model, true);
+        },
+
+        refreshModel: function (model) {
+            var existing = this.get('#chat-' + model.cid);
+            var refreshed = domify(this.chatHTML(model, true));
+            existing.parentElement.replaceChild(refreshed, existing);
+        },
+
+        appendModel: function (model, preload) {
+            var self = this;
+            var useExistingGroup = this.chatShouldGroupWith(model, this.lastModel);
+            var newEl, first, last, items;
+
+            newEl = domify(this.chatHTML(model, useExistingGroup));
+
+            if (useExistingGroup) {
+                items = this.$messageList.getElementsByTagName('li');
+                last = items[items.length - 1];
+                last.getElementsByClassName('messageWrapper')[0].appendChild(newEl);
+                this.staydown.checkdown();
+            } else {
+                this.staydown.append(newEl);
+            }
+
+            this.lastModel = model;
+        },
+
+        resizeInput: _.throttle(function () {
+            var height;
+            var scrollHeight;
+            var newHeight;
+            var newPadding;
+            var paddingDelta;
+            var maxHeight = 102;
+
+            this.$chatInput.removeAttribute('style');
+            height = this.$chatInput.clientHeight;
+            scrollHeight = this.$chatInput.scrollHeight;
+            newHeight = scrollHeight + 2;
+
+            if (newHeight > maxHeight) newHeight = maxHeight;
+            if (newHeight > height) {
+                this.$chatInput.style.height = "" + newHeight + "px";
+                newPadding = newHeight + 21;
+                paddingDelta = newPadding - parseInt(this.$messageList.style.paddingBottom, 10);
+                if (!!paddingDelta) {
+                    this.$messageList.style.paddingBottom = "" + newPadding + "px";
+                }
+            }
+        }, 300),
+
+        chatHTML: function (msg, useExistingGroup) {
+            var classes = [];
+
+            if (msg.mine) classes.push('mine');
+            if (msg.pendingAck) classes.push('pendingAck');
+            if (msg.delayed) classes.push('delayed');
+            if (msg.edited) classes.push('edited');
+            if (msg.pendingReceipt) classes.push('pendingReceipt');
+            if (msg.receiptReceived) classes.push('delivered');
+            if (msg.meAction) classes.push('meAction');
+
+
+            if (useExistingGroup) {
+                return templates.chat({message: msg, classList: classes});
+            } else {
+                return templates.chatGroup({message: msg, classList: classes});
+            }
+        },
+
+        chatShouldGroupWith: function (msg, prevMsg) {
+            if (msg.type === 'groupchat') {
+                return !!prevMsg && prevMsg.fromFullJID === msg.fromFullJID;
+            } else {
+                return !!prevMsg && prevMsg.fromBareJID === msg.fromBareJID;
+            }
+        },
+
+        sendChat: function () {
+            var val = this.$chatInput.value;
+
+            if (val) {
+                this.staydown.intend_down = true;
+
+                if (this.state.editing) {
+                    this.model.sendChat(val, this.model.lastSentMessageID);
+                } else {
+                    this.model.sendChat(val);
+                }
+            }
+
+            this.state.editing = false;
+            this.state.typing = false;
+            this.state.paused = false;
+            this.$chatInput.value = '';
+        }
+    });
+};
